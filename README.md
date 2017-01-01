0c02b8231e437ea0599b1b547ebf34da9e3aeace
Emad Rahman <emadpoke@gmail.com>
2016-12-31 17:03:30 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+I am learning java. I have no prior java knowledge however I am familiar with
+other programming languages such as c++
---
+++ testing/first.java
@@ -0,0 +1,10 @@
+class apples{
+  public static void main(String Args[]){
+    System.out.println("Hello World!");
+
+    int num = 20;
+
+    String result = (num < 12) ? "Less" : "More";
+    System.out.println(result);
+  }
+}
